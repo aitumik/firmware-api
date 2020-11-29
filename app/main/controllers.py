79728586178adf96f0b5756f6c03393e4bca5c4f
Nathan <nathanpykimutai@gmail.com>
2020-11-30 00:58:05 +0300

    added chartjs for creating charts

--- app/main/controllers.py
+++ app/main/controllers.py
@@ -1,7 +1,10 @@
 from flask import Blueprint,request,jsonify,render_template
 
+from app.api.models import Data
+
 main = Blueprint('main',__name__)
 
 @main.route("/")
 def index():
-    return render_template("index.html")
+    data = [d.to_json() for d in Data.query.all()[:100]]
+    return render_template("index.html",data=data)
--- app/static/custom.js
+++ app/static/custom.js
@@ -1,3 +1,10 @@
 $(document).ready(function() {
   console.log("Working");
 });
+
+function load_data() {
+  console.log("Nothing");
+}
+
+
+
