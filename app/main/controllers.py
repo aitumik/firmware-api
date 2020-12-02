d3944a6267ea9e83734068eb557e0386ef0e09f0
Nathan <nathanpykimutai@gmail.com>
2020-12-03 01:16:44 +0300

    changed some stuff

--- app/main/controllers.py
+++ app/main/controllers.py
@@ -9,6 +9,12 @@ def index():
     data = [d.to_json() for d in Data.query.all()]
     return render_template("index.html",data=data)
 
+@main.route("/humidity")
+def humidity():
+    data = [d.to_json() for d in Data.query.all()]
+    return render_template("humidity.html",data=data)
+
+
 @main.route("/test")
 def test():
     return render_template("test.html")
