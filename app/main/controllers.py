39352b780416415eaa68389627a0bbd16eb1600e
Nathan <nathanpykimutai@gmail.com>
2020-12-03 08:53:12 +0300

    finished working on the ui firmware

--- app/main/controllers.py
+++ app/main/controllers.py
@@ -15,8 +15,27 @@ def humidity():
     data = [d.to_json() for d in Data.query.all()]
     return render_template("humidity.html",data=data)
 
-@main.route("/pastdata/<minutes>",methods=['GET','POST'])
-def past_data(minutes):
+@main.route("/pastdata/humidity/<minutes>",methods=['GET','POST'])
+def past_humid(minutes):
+    offset = datetime.now() - timedelta(minutes=int(minutes))
+    data = Data.query.all()
+    data = [d for d in data if d.timestamp < offset]
+    if len(data) > 50:
+        data = data[len(data) - 50:len(data)]
+
+    labels = [str(j.timestamp).split(" ")[1].split(".")[0] for j in data]
+    values = [j.humidity for j in data]
+
+    colors = [
+    "#F7464A", "#46BFBD", "#FDB45C", "#FEDCBA",
+    "#ABCDEF", "#DDDDDD", "#ABCABC", "#4169E1",
+    "#C71585", "#FF4500", "#FEDCBA", "#46BFBD"]
+
+    return render_template("previous_data.html",title="Previous \
+            Humidity Data",max=100,labels=labels,values=values)
+
+@main.route("/pastdata/temperature/<minutes>",methods=['GET','POST'])
+def past_temp(minutes):
     offset = datetime.now() - timedelta(minutes=int(minutes))
     data = Data.query.all()
     data = [d for d in data if d.timestamp < offset]
@@ -31,10 +50,9 @@ def past_data(minutes):
     "#ABCDEF", "#DDDDDD", "#ABCABC", "#4169E1",
     "#C71585", "#FF4500", "#FEDCBA", "#46BFBD"]
 
-    return render_template("previous_data.html",title="Previous \
-            Data",max=100,labels=labels,values=values)
+    return render_template("previous_temperature.html",title="Previous \
+            Temperature Data",max=60,labels=labels,values=values)
 
 @main.route("/test")
 def test():
     return render_template("test.html")
-
