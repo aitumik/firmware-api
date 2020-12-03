7a618174557c788bbac30e55b9ea1e414bffc3cc
Nathan <nathanpykimutai@gmail.com>
2020-12-03 06:07:14 +0300

    changed again

--- app/static/custom.js
+++ app/static/custom.js
@@ -58,11 +58,7 @@ function addData(myChart,labels,values) {
     labels: labels,
     datasets: [{
       label: "Temperature",
-      data: values,
-      fillColor: "rgba(151,249,190,0.5)",
-      strokeColor: "rgba(255,255,255,1)",
-      pointColor: "rgba(220,220,220,1)",
-      pointStrokeColor: "#fff",
+      backgroundColor: '#af90ca',
     }],
   };
   myChart.data = d;
